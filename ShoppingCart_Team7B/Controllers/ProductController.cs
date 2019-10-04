db58c92723fa9fbd32584b16a9e70bfd5cd1e274
Wei Zheng <wzyeo1990@gmail.com>
2019-10-04 14:34:39 +0800

    list products

--- ShoppingCart_Team7B/Controllers/ProductController.cs
+++ ShoppingCart_Team7B/Controllers/ProductController.cs
@@ -14,79 +14,12 @@ namespace ShoppingCart_Team7B.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult ListProducts()
         {
-            /*
-                var db = new ShoppingCartDbContext();
-
-                List<Product> ListProduct = new List<Product>();
-                
-                      var details = new Product();
-
- = 
-
-                        details.ProductId = (int)["ProductId"];
-                        details.ProductName = (string) ["ProductName"].ToString();
-                        details.Price = (double)["Price"];
-                        details.Description =["Description"].ToString();
-                        
-                    
-
-                        ListProduct.Add(details);
-
-                    }  ViewData["details"] = ListProduct;
-
-                } return View();
-            } */
             var db = new ShoppingCartDbContext();
             var productList = db.Product.ToList();
             ViewData["productList"] = productList;      
-            /*
-              using (SqlConnection connection=new SqlConnection("Server=wzyeo.net; Database=ShoppingCart_Team7B; User Id=Team7B; Password=Team7BRocks"))
-              {
-                  connection.Open();
-                  string sql = @"SELECT Products.ProductId, Products.ProductName, Products.Price, Products.Description
-                              FROM Products";
-                  SqlCommand cmd = new SqlCommand(sql, connection);
-                  SqlDataReader reader = cmd.ExecuteReader();
-
-                  List<Product> ListProduct = new List<Product>();
-
-                  while (reader.Read())
-                  {
-                      var details = new Product();
-
-                      details.ProductId = (int)reader["ProductId"];
-                      details.ProductName = reader["ProductName"].ToString();
-                      details.Price = (double)reader["Price"];
-                      details.Description = reader["Description"].ToString();
-
-                      ListProduct.Add(details);
-                  }
-
-                  ViewData["details"] = ListProduct;
-
-              } */
             return View(); 
-              
-
-
-        }
-        public ActionResult LogOut() // to link to User Login Page
-        {
-            
-            return RedirectToAction("Login","Login");
-        }
-
-        public ActionResult Checkout()
-        {
-            return RedirectToAction("ViewPurchases", "Purchases");
         }
     }
 }
--- ShoppingCart_Team7B/Database/ShoppingCartDbInitializer.cs
+++ ShoppingCart_Team7B/Database/ShoppingCartDbInitializer.cs
@@ -13,11 +13,17 @@ namespace ShoppingCart_Team7B.Database
         {
             User user1 = new User("admin", "admin");
             Product product1 = new Product(".NET Charts", 99, "Brings powerful charting capabilities to your .NET applications.");
+            Product product2 = new Product(".NET PayPal", 69, "Integrate your .NET apps with PayPal the easy way!");
+            Product product3 = new Product(".NET ML", 299, "Supercharged .NET machine learning libraries.");
+            Product product4 = new Product(".NET Analytics", 299, "Performs data mining and analytics easily in .NET.");
             user1.Cart = new Cart();
             user1.Cart.CartGroups.Add(new CartGroup(2, product1));
 
             context.User.Add(user1);
             context.Product.Add(product1);
+            context.Product.Add(product2);
+            context.Product.Add(product3);
+            context.Product.Add(product4);
             base.Seed(context);
         }
     }
