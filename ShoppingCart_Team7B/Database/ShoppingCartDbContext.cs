9e49ef38e40ea3850737d60fd2fb38058daafe52
Wei Zheng <30838747+yeoweizheng@users.noreply.github.com>
2019-11-12 10:08:27 +0800

    Update ShoppingCartDbContext.cs

---
+++ ShoppingCart_Team7B/Database/ShoppingCartDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ShoppingCart_Team7B.Models;
+using ShoppingCart_Team7B.Database;
+
+namespace ShoppingCart_Team7B.Database
+{
+    public class ShoppingCartDbContext:DbContext
+    {
+        public ShoppingCartDbContext() : base("Server=******; Database=ShoppingCart_Team7B; User Id=******; Password=******")
+        {
+            System.Data.Entity.Database.SetInitializer(new ShoppingCartDbInitializer());
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartGroup> CartGroup { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
+        public DbSet<PurchaseGroup> PurchaseGroup { get; set; }
+        public DbSet<ActivationCode> ActivationCode { get; set; }
+        public DbSet<Session> Session { get; set; }
+    }
+}
